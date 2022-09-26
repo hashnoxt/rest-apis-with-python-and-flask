9129e74b0fc7a5bd24600e51fefd23a507fcb982
hashan nalaka <hashannalaka0123@gmail.com>
2022-09-26 06:59:07 +0530

    server/app : added users
    IN-07

--- 05/code/security.py
+++ 05/code/security.py
@@ -1,33 +1,32 @@
-
 from user import User
 
-users = [
-    User(1, 'bob', 'asdf')
-]
 
-username_mapping = {u.username: u for u in users}
-#     {'bob': {
-#     'id': 1,
-#     'username': 'bob',
-#     'password': 'asdf',
-# }}
+# users = [
+#     User(1, 'bob', 'asdf')
+# ]
 
-userid_mapping = {u.id: u for u in users}
-
-
-# {1: {
-#     'id': 1,
-#     'username': 'bob',
-#     'password': 'asdf',
-# }}
-
+# username_mapping = {u.username: u for u in users}
+# #     {'bob': {
+# #     'id': 1,
+# #     'username': 'bob',
+# #     'password': 'asdf',
+# # }}
+#
+# userid_mapping = {u.id: u for u in users}
+#
+#
+# # {1: {
+# #     'id': 1,
+# #     'username': 'bob',
+# #     'password': 'asdf',
+# # }}
 
 def authenticate(username, password):
-    user = username_mapping.get(username, None)
+    user = User.find_by_username(username)  # username_mapping.get(username, None)
     if user and user.password == password:
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    return User.find_by_id(user_id)  # userid_mapping.get(user_id, None)
