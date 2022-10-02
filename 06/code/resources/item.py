a0b222bc98377f6362d3981640683295330be75e
hashan nalaka <hashannalaka0123@gmail.com>
2022-10-02 10:40:38 +0530

    server/app : final commit
    IN-08

--- 06/code/resources/item.py
+++ 06/code/resources/item.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 import sqlite3
+from models.item import ItemModel
 
 
 class Item(Resource):
@@ -11,64 +12,44 @@ class Item(Resource):
         required=True,
         help="This field cannot be left"
     )
+    parser.add_argument(
+        'store_id',
+        type=int,
+        required=True,
+        help="Every item needs a store id."
+    )
 
     @jwt_required()
     def get(self, name):
 
-        item = self.find_by_name(name)
+        item = ItemModel.find_by_name(name)
         if item:
-            return item
+            return item.json()
         return {'message': 'Item not found'}, 404
-
-    @classmethod
-    def find_by_name(cls, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = 'SELECT * FROM items WHERE name=?'
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
-        connection.close()
-
-        if row:
-            return {'item': {'name': row[0], 'price': row[1]}}, 200
 
     def post(self, name):
 
-        if self.find_by_name(name):
+        if ItemModel.find_by_name(name):
             return {"message': An item with name '{}' already exists.".format(name)}, 400
 
         data = Item.parser.parse_args()
-        item = {'name': name, 'price': data['price']}
+
+        item = ItemModel(name, **data)  # data['price'], data['store_id'])
 
         try:
-            self.insert(item)
+            item.save_to_db()
+
         except:
             return {'message': 'An error occured'}, 500  # internal server error
 
-        return item, 201
-
-    @classmethod
-    def insert(cls, item):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO items VALUES (?, ?)"
-        cursor.execute(query, (item['name'], item['price']))
-
-        connection.commit()
-        connection.close()
+        return item.json(), 201
 
     def delete(self, name):
 
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+        item = ItemModel.find_by_name(name)
 
-        query = "DELETE FROM items WHERE name=?"
-        cursor.execute(query, (name,))
-
-        connection.commit()
-        connection.close()
+        if item:
+            item.delete_from_db()
 
         return {'message': 'Item deleted'}
 
@@ -76,46 +57,22 @@ class Item(Resource):
 
         data = Item.parser.parse_args()
 
-        item = self.find_by_name(name)
-        updated_item = {'name': name, 'price': data['price']}
-
+        item = ItemModel.find_by_name(name)
 
         if item is None:
             try:
-                self.insert(updated_item)
+                item = ItemModel(name, **data)
+
             except:
                 return {'message': 'An error occured inserting the item'}, 500
         else:
-            try:
-                self.update(updated_item)
-            except:
-                return {'message': 'An error occured inserting the item'}, 500
+            item.price = data['price']
 
-        return updated_item
-
-    @classmethod
-    def update(cls, item):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "UPDATE items SET price=? WHERE name=?"
-        cursor.execute(query, (item['price'], item['name']))
-
-        connection.commit()
-        connection.close()
+        item.save_to_db()
+        return item.json()
 
 
 class ItemList(Resource):
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
-        items = []
-        for row in result:
-            items.append({'name': row[0], 'price': row[1]})
-
-        connection.close()
-
-        return {'items': items}
+        return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
+        # [ItemModel.query.all() for item in ItemModel.query.all()]}
--- 06/code/security.py
+++ 06/code/security.py
@@ -1,12 +1,12 @@
-from resources.user import User
+from models.user import UserModel
 
 
 def authenticate(username, password):
-    user = User.find_by_username(username)  # username_mapping.get(username, None)
+    user = UserModel.find_by_username(username)  # username_mapping.get(username, None)
     if user and user.password == password:
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return User.find_by_id(user_id)  # userid_mapping.get(user_id, None)
+    return UserModel.find_by_id(user_id)  # userid_mapping.get(user_id, None)
